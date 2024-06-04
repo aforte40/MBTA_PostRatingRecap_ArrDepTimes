c6ba2215e4eba82a33725041108ee6fb6326b5c5
aforte40 <antonio.forte@polito.it>
2024-06-04 18:20:22 -0400

    Finalized import routine
    Converted all intermediate steps to functions. Introduce I/E via pickle, as it literally takes a couple of seconds to load all the dataset

--- layover_functions.py
+++ layover_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime as dt
+import numpy as np
 
 # Compute layover times from real-time dataframe 
 def compute_layover_times(original_df):
@@ -35,4 +36,32 @@ def reshape_for_layover(df_with_layover_times):
     route_stop_ids = pivot_table.columns.tolist()
     # Reset index to turn multi-index into columns
     pivot_table.reset_index(inplace=True)
+    # Remove index names
+    pivot_table.index.name = None
+    pivot_table.columns.name = None
     return route_stop_ids, pivot_table
+
+# Define the function to update layover_dict
+def update_layover_dict(layover_dict, route_id, stop_ids, layover_df):
+    route_id_key = f'route_{route_id}'
+    stop_id_key = f'stop_ids_route{route_id}'
+    layover_df_key = f'layover_df_route{route_id}'
+
+    # If the route_id is not in the dictionary, add it
+    if route_id_key not in layover_dict:
+        layover_dict[route_id_key] = {stop_id_key: [], layover_df_key: pd.DataFrame()}
+        
+    # Append stop_ids to the stop_ids list in the dictionary
+    current_stop_ids = layover_dict[route_id_key][stop_id_key]
+    new_stop_ids = [stop_id for stop_id in stop_ids if stop_id not in current_stop_ids]
+    layover_dict[route_id_key][stop_id_key].extend(new_stop_ids)
+
+    # Update the DataFrame in the dictionary
+    existing_df = layover_dict[route_id_key][layover_df_key]
+    if not existing_df.empty and new_stop_ids:
+        # Add columns for new stop_ids
+        for stop_id in new_stop_ids:
+            existing_df[stop_id] = np.nan
+    # Concatenate the new DataFrame
+    updated_df = pd.concat([existing_df, layover_df], axis=0)
+    layover_dict[route_id_key][layover_df_key] = updated_df
